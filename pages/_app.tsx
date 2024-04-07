23d60938b0643807bceb7d660c6376e9a2f15831
zsoltibv <zsoltibv@gmail.com>
2024-04-07 14:47:19 +0300

    added loader bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import NProgress from "nprogress";
+import Router from "next/router";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const client = new ApolloClient({
   uri: "https://api.jobdating.ro?graphql",
