a1cddb9d9b416de1bb32689b60bfd9323320c6fa
zsoltibv <zsoltibv@gmail.com>
2024-06-19 14:20:29 +0300

    small fix

--- components/pageHeroSections.tsx
+++ components/pageHeroSections.tsx
@@ -9,7 +9,7 @@ export default function PageHeroSection({ image, page }) {
       <div className="absolute inset-0 bg-zinc-700 bg-opacity-65 z-0"></div>
       <div className="absolute inset-0 z-10 flex justify-center items-center">
         <div className="container w-full mx-auto text-left px-4 pt-6">
-          <div className="bg-cyan-500 bg-opacity-80 md:p-8 p-6 rounded">
+          <div className="bg-cyan-500 bg-opacity-80 md:p-8 p-4 rounded">
             <h1 className="text-white font-open-sans md:text-5xl text-2xl font-regular md:mb-4 mb-2">
               {page.title}
             </h1>
