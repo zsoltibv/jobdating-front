a96e004905d866d6259805d5e955bcd10538500f
zsoltibv <zsoltibv@gmail.com>
2024-09-22 16:01:20 +0300

    update message

--- components/jobSectionGridCategories.tsx
+++ components/jobSectionGridCategories.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const JobSectionGridCategories = ({ jobs }) => {
   if (!jobs) {
-    return <div>No jobs found</div>;
+    return <div>Niciun job găsit</div>;
   }
 
   return (
